3cfb8944d078d14b6f2472ea453272d8bb63243a
mbhuet <mbhuet@gmail.com>
2015-01-22 15:05:06 -0500

    Project Submission Build

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,257 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Player owner;
+	public Vector3 moveDirection;
+	public bool isNeutral = true;
+	private float speed = 10;
+	GameObject arrow;
+	TrailRenderer trail;
+	public float playerInfluence;
+
+	bool isHeld = false;
+
+	public AudioClip hit;
+	public AudioClip wallDeflect;
+
+	Vector3 homePos;
+	protected LineCircle ring;
+	bool isHome = true;
+
+	
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (LayerMask.NameToLayer("Wall") == col.collider.gameObject.layer) {
+
+			if(col.gameObject.tag == "Top"){
+				Deflect(Vector3.down);
+			}
+			
+			if(col.gameObject.tag == "Bottom"){
+				Deflect(Vector3.up);
+
+			}
+			
+			if(col.gameObject.tag == "Left"){
+				Deflect(Vector3.right);
+				SetNeutral();
+
+
+			}
+			
+			if(col.gameObject.tag == "Right"){
+				Deflect(Vector3.left);
+				SetNeutral();
+
+
+			}
+			Camera.main.audio.PlayOneShot(wallDeflect);
+
+		}
+		if (LayerMask.NameToLayer("Player") == col.collider.gameObject.layer) {
+			Player other = col.gameObject.GetComponent<Player>();
+			Vector3 normal = (this.transform.position - other.transform.position).normalized;
+
+			if (isNeutral){
+				Deflect(normal);
+				Camera.main.audio.PlayOneShot(wallDeflect);
+
+			}
+			else if (other.team == owner.team){
+				Deflect(normal);
+				Camera.main.audio.PlayOneShot(wallDeflect);
+
+			}
+			else if (other.team != owner.team){
+				other.DecrementHealth();
+					Deflect(normal);
+					SetNeutral();
+				Camera.main.audio.PlayOneShot(hit);
+			}
+		}
+
+		if (LayerMask.NameToLayer("Ball") == col.collider.gameObject.layer) {
+			Ball other = col.gameObject.GetComponent<Ball>();
+			Vector3 normal = (this.transform.position - other.transform.position).normalized;
+			
+			if (other.isNeutral){
+				//Physics2D.IgnoreCollision();
+				Deflect(normal);
+				Camera.main.audio.PlayOneShot(wallDeflect);
+				
+			}
+			else if (!this.isNeutral){
+			if (other.owner.team == owner.team){
+				Deflect(normal);
+				Camera.main.audio.PlayOneShot(wallDeflect);
+				
+			}
+			else if (other.owner.team != owner.team){
+				Deflect(normal);
+				Camera.main.audio.PlayOneShot(wallDeflect);
+			}
+			}
+		}
+
+			
+	}
+
+
+	void SetColor(Color color){
+		renderer.material.color = color;
+		arrow.renderer.material.color = color;
+		trail.renderer.material.color = color;
+		particleSystem.startColor = color;
+	}
+
+	public void SetOwner(Player p){
+		isHome = false;
+		this.gameObject.layer = LayerMask.NameToLayer ("Ball");
+		owner = p;
+		SetColor (owner.color);
+
+		isNeutral = false;
+		isHeld = true;
+		collider2D.enabled = false;
+		trail.enabled = false;
+		transform.parent = owner.transform;
+		transform.localPosition = Vector3.right * .8f;
+		transform.localRotation = Quaternion.Euler(0,0,-90);
+		rigidbody2D.isKinematic = true;
+	}
+
+	void MoveBall(){
+
+		if (owner != null) {
+						moveDirection += owner.moveVector * playerInfluence;
+			if (moveDirection.magnitude < .5f){
+				moveDirection = moveDirection.normalized * .5f;
+			}
+				} else
+						moveDirection *= (1-Time.fixedDeltaTime * .5f);
+
+		if (moveDirection.magnitude > 3){
+			moveDirection = moveDirection.normalized * 3;
+			
+		}
+
+		Vector3 move = moveDirection * Time.fixedDeltaTime * speed;
+		
+
+		this.rigidbody2D.MovePosition (this.transform.position + move);
+		if (moveDirection.magnitude > .01f) {
+			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle -90, Vector3.forward);
+				} 
+		if (moveDirection.magnitude < .1f && isNeutral && !isHome) {
+			StartCoroutine("ReturnHome");
+		}
+	}
+
+	public void SetNeutral(){
+		isNeutral = true;
+		owner = null;
+		isHeld = false;
+		SetColor(Color.white);
+		this.gameObject.layer = LayerMask.NameToLayer ("DeadBall");
+	}
+
+	// Use this for initialization
+	void Start () {
+		ring = transform.FindChild ("Ring").GetComponent<LineCircle>();
+		ring.SetThickness (.075f);
+		//ring.SetRadius (this.transform.localScale.x);
+
+		homePos = this.transform.position;
+		//moveDirection = Vector3.one - Vector3.forward;
+		arrow = transform.FindChild ("Arrow").gameObject;
+		trail = transform.FindChild ("Trail").gameObject.GetComponent<TrailRenderer> ();
+		SetNeutral ();
+
+	}
+	void Update(){
+
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (owner != null && owner.isDead){ 
+			Debug.Log("neutralize ball, owner is dead");
+			SetNeutral();
+		}
+
+		if (!isHeld) {
+			MoveBall ();
+				}
+	}
+
+	void ResetPosition(){
+		transform.position = Vector3.zero;
+		moveDirection = Vector3.zero;
+	}
+
+	public void Deflect(Vector3 normal){
+		//Vector3 deflectionPoint3 = new Vector3 (deflectionPoint.x, deflectionPoint.y, 0);
+		Vector3 reflection = Vector3.Reflect (moveDirection, normal);
+		moveDirection = reflection;
+//		Debug.Log (reflection);
+	}
+	/*
+	public void Deflect(GameObject other){
+		//Vector3 deflectionPoint3 = new Vector3 (deflectionPoint.x, deflectionPoint.y, 0);
+		Vector3 reflection = Vector3.Reflect (moveDirection, normal);
+		moveDirection = reflection;
+		//		Debug.Log (reflection);
+	}
+	*/
+
+	public void Release(Vector3 direction){
+		isHeld = false;
+		collider2D.enabled = true;
+		trail.enabled = true;
+		transform.parent = null;
+		transform.localRotation = Quaternion.Euler(0,0,-90);
+		rigidbody2D.isKinematic = false;
+		moveDirection = direction;
+	}
+
+	IEnumerator ReturnHome(){
+		isHome = true;
+
+		float effectSpeed = 2;
+		collider2D.enabled = false;
+		ring.SetRadius (1);
+
+		trail.time = -1;
+		trail.enabled = false;
+
+		float t = this.transform.localScale.x;
+		while (t > 0) {
+			t-=Time.deltaTime * effectSpeed;
+			this.transform.localScale = Vector3.one * t;
+			trail.startWidth = t;
+			yield return null;
+		}
+		t = 0;
+
+
+		yield return null;
+
+		this.transform.position = homePos;
+		moveDirection = Vector3.zero;
+		transform.rotation = Quaternion.identity;
+		while (t < .5f) {
+			t += Time.deltaTime * effectSpeed;	
+			this.transform.localScale = Vector3.one * t;
+			yield return null;
+		}
+		this.transform.localScale = Vector3.one * .5f;
+		trail.startWidth = .5f;
+//		Debug.Log ("here");
+		ring.SetRadius (0);
+		collider2D.enabled = true;
+		trail.enabled = true;
+		trail.time = .3f;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,362 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+
+public enum Team{ONE, TWO};
+
+public class Player : MonoBehaviour {
+	public int playerNum;
+
+	GameObject visual;
+
+	public Team team;
+
+	public int health = 3;
+	public float speed;
+	public Ball heldBall;
+	public bool isThrowing;
+	public float scaleFactor;
+	public bool againstWall;
+	public Vector3 moveVector;
+	public Color color;
+	private LineCircle[] healthBalls;
+	private MeshFilter[] dmgBalls;
+
+	float defenseRadius = 1.4f;
+	protected PlayerIndex gamepadNum;
+	protected GamePadState gamepad;
+	protected LineCircle ring;
+
+	bool defenseAvailable = true;
+
+	public GameObject[] healthBars;
+	public AudioClip throwSound;
+	public AudioClip pickupSound;
+	public AudioClip catchSound;
+
+	public bool isDead = false;
+
+	
+	// Use this for initialization
+	void Start () {
+		Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gamepad = GamePad.GetState(gamepadNum);
+
+		if (playerNum == 0){
+			if (Input.GetButtonDown("Fire1")){
+				Throw ();
+			}
+			if (Input.GetButtonDown("Fire2") && defenseAvailable){
+				//Debug.Log("Fire2 down");
+				//StartCoroutine ("ActiveDefense");
+				Catch ();
+				StartCoroutine("DefenseCooldown");
+
+
+			}
+			if (Input.GetButtonUp("Fire2") && !defenseAvailable && defenseRadius >0){
+				//Debug.Log("Fire2 up");
+
+				//Catch ();
+				//StopCoroutine("ActiveDefense");
+				//StartCoroutine("DefenseCooldown");
+			}
+		}
+		else{
+			if (gamepad.Buttons.A == ButtonState.Pressed){
+				Throw ();
+			}
+			if (gamepad.Buttons.B == ButtonState.Pressed && defenseAvailable){
+				Catch ();
+				StartCoroutine("DefenseCooldown");
+
+				//StartCoroutine ("ActiveDefense");
+			}
+			if (gamepad.Buttons.B == ButtonState.Released && !defenseAvailable  && defenseRadius >0){
+				//Catch ();
+				//StopCoroutine("ActiveDefense");
+				//StartCoroutine("DefenseCooldown");
+			}
+		}
+	}
+
+	void FixedUpdate(){
+		Move ();
+	}
+
+	void SetColor(Color c){
+		//renderer.material.color = color;
+		visual.renderer.material.color = color;
+		particleSystem.startColor = color;
+	}
+
+	void Throw(){
+		if(heldBall != null){
+			heldBall.Release (this.transform.right);
+			heldBall = null;
+			PlayThrowSound();
+		}
+	}
+
+	void Block(Ball ball){
+
+	}
+
+	void Catch(){
+		Collider2D[] objectsInRing = Physics2D.OverlapCircleAll (new Vector2 (transform.position.x, transform.position.y), defenseRadius + .1f);
+		foreach (Collider2D obj in objectsInRing) {
+			if (obj.gameObject.tag == "Ball"){
+				//StartCoroutine("SlowMotion", .1f);
+				Ball ball  = obj.GetComponent<Ball>();
+				//catching an opponent's ball
+				if(!ball.isNeutral && ball.owner.team != this.team){
+					Camera.main.audio.PlayOneShot (catchSound);
+					ball.particleSystem.Emit(10);
+					if (heldBall == null){
+						Pickup(ball);
+						RecoverHealth();
+					}
+					else{
+						ball.Deflect(ball.transform.position - this.transform.position);
+						ball.SetNeutral();
+
+					}
+				}
+				/*
+				if (ball.isNeutral){
+				}
+				else{
+					if (heldBall == null){
+						ball.particleSystem.Emit(10);
+						//Camera.main.audio.PlayOneShot (catchSound);
+
+						Pickup(ball);
+					}
+					else{ 
+						ball.Deflect(ball.transform.position - this.transform.position);
+						//Camera.main.audio.PlayOneShot (catchSound);
+
+						ball.particleSystem.Emit(10);
+						ball.SetNeutral();
+					}
+				}
+				*/
+			}
+		}
+	}
+
+	void Pickup(Ball b){
+//		Debug.Log("Pickup Ball");
+		PlayPickupSound ();
+		heldBall = b;
+		heldBall.SetOwner (this);
+	}
+
+	void Move(){
+		float leftX, leftY;
+		if (playerNum == 0){
+			leftX = Input.GetAxis ("Horizontal");
+			leftY = Input.GetAxis ("Vertical");
+		}
+		else{
+			leftX = gamepad.ThumbSticks.Left.X;
+			leftY = gamepad.ThumbSticks.Left.Y;
+		}
+		moveVector = new Vector3 (leftX, leftY, 0);
+		this.rigidbody2D.MovePosition (this.transform.position + moveVector * Time.fixedDeltaTime * speed);
+	}
+
+
+
+	public void Init () {
+		switch (playerNum){
+		case 1: gamepadNum = PlayerIndex.One;
+			Debug.Log("Player 1 Registered");
+			break;
+		case 2: gamepadNum = PlayerIndex.Two;
+			Debug.Log("Player 2 Registered");
+			break;
+		case 3: gamepadNum = PlayerIndex.Three;
+			Debug.Log("Player 3 Registered");
+			break;
+		case 4 : gamepadNum = PlayerIndex.Four;
+			Debug.Log("Player 4 Registered");
+			break;
+		default :
+			playerNum = 0;
+			break;
+		}
+		name = ("Player " + playerNum);
+		//healthBalls = new LineCircle[health];
+		//dmgBalls = new MeshFilter[health];
+		DisplayHealth();
+
+		if (this.transform.position.x > 0) {
+			transform.Rotate(0,0,180);
+		} 
+		else {
+			transform.FindChild("Balls").GetComponent<Transform>().Rotate(0,0,180);
+			Vector3 currPos = transform.FindChild("Balls").GetComponent<Transform>().localPosition;
+			Vector3 newPos = new Vector3(currPos.x, -currPos.y, currPos.z);
+
+			transform.FindChild("Balls").GetComponent<Transform>().localPosition = newPos;
+
+		}
+		visual = transform.FindChild ("Visual").gameObject;
+
+		SetColor (color);
+		ring = transform.FindChild ("Ring").GetComponent<LineCircle>();
+		ring.SetRadius (defenseRadius);
+
+	}
+
+	void PlayThrowSound(){
+		Camera.main.audio.PlayOneShot (throwSound);
+		//AudioSource.PlayClipAtPoint(throwSound, transform.position);
+//		Debug.Log("throw sound");
+	}
+	
+	void PlayPickupSound(){
+		Camera.main.audio.PlayOneShot (pickupSound);
+		//AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+		Debug.Log("pickup sound");
+	}
+	
+	//function to display health as tiny circles within the player
+	void DisplayHealth(){
+		//draw 3 circles
+		/*
+		healthBalls[0] = transform.FindChild("HPball0").GetComponent<LineCircle>();
+		healthBalls[1] = transform.FindChild("HPball1").GetComponent<LineCircle>();
+		healthBalls[2] = transform.FindChild("HPball2").GetComponent<LineCircle>();
+		dmgBalls[0] = transform.FindChild("Balls").FindChild("black0").GetComponent<MeshFilter>();
+		dmgBalls[1] = transform.FindChild("black1").GetComponent<MeshFilter>();
+		dmgBalls[2] = transform.FindChild("black2").GetComponent<MeshFilter>();*/
+
+		for(int i = 0; i < 3; i++){
+			if (i<health)
+			healthBars[i].renderer.enabled = false;
+			else
+				healthBars[i].renderer.enabled = true;
+
+		}
+	}
+
+
+	public void DecrementHealth(){
+		//makes one of the circles black
+		health--;
+		particleSystem.Emit(10);
+		DisplayHealth ();
+		if (health < 0) {
+			//Throw ();
+			StartCoroutine("Death");		
+		}
+	}
+
+	public void RecoverHealth(){
+		if (health < 3) {
+						health++;
+				}
+		DisplayHealth ();
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Ball"){
+			Ball ball = other.GetComponent<Ball>();
+			if (ball.isNeutral && heldBall == null){
+				Pickup(ball);
+			}
+			
+		}
+	}
+
+	IEnumerator ActiveDefense(){
+		float maxRadius = 1.25f;
+		float growSpeed = 2;
+		//Debug.Log ("Activate Defense");
+		defenseAvailable = false;
+		defenseRadius = .55f;
+		ring.SetRadius(defenseRadius);
+		while (defenseRadius < maxRadius) {
+			defenseRadius += Time.deltaTime * growSpeed;
+			ring.SetRadius(defenseRadius);
+			yield return null;
+		}
+		if (defenseRadius >= maxRadius) {
+						defenseRadius = maxRadius;
+						ring.SetRadius (defenseRadius);
+						yield return new WaitForSeconds (.3f);
+		}
+		//Debug.Log("after");
+		defenseRadius = 0;
+		ring.SetRadius (defenseRadius);
+
+		StartCoroutine ("DefenseCooldown");
+	}
+
+	IEnumerator DefenseCooldown(){
+		defenseAvailable = false;
+		float effectSpeed = 10;
+		//float maxDefenseRadius = defenseRadius;
+		//Debug.Log("DefenseCooldown");
+		float t = defenseRadius;
+		//defenseRadius = 0;
+
+		while (t > .55f) {
+			t -= Time.deltaTime * effectSpeed;
+			ring.SetRadius(t);
+			yield return null;
+		}
+		ring.SetRadius (.55f);
+		t = .55f;
+		yield return new WaitForSeconds (1);
+
+		while (t < defenseRadius) {
+			t += Time.deltaTime * effectSpeed;
+			ring.SetRadius(t);
+			yield return null;
+		}
+
+		ring.SetRadius (defenseRadius);
+		defenseAvailable = true;
+		//Debug.Log ("Defense Available");
+	}
+
+	IEnumerator SlowMotion(float scale){
+		Time.timeScale = 0;
+		while (Time.timeScale < 1) {
+			Time.timeScale += Time.unscaledDeltaTime * scale;
+			yield return null;
+		}
+
+		Time.timeScale = 1;
+	}
+
+	IEnumerator Death(){
+		isDead = true;
+		Throw ();
+		this.enabled = false;
+		//GameObject.Destroy (this.collider2D);
+		//GameObject.Destroy (this.collider2D);
+		foreach (CircleCollider2D col in transform.GetComponents<CircleCollider2D> ()) {
+			col.enabled = false;		
+		}
+		//collider2D.enabled = false;
+		visual.renderer.enabled = false;
+		ring.gameObject.SetActive (false);
+		transform.FindChild ("Balls").gameObject.SetActive (false);
+
+		yield return new WaitForSeconds (2);
+
+		GameObject.Destroy (this.gameObject);
+
+	}
+	
+}
